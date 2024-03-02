26ef22092d5382c64b90cb966e5e9a34ab4b16e2
JessiTejada05 <tejadajessi1@gmail.com>
2024-03-02 10:38:21 -0600

    Ejercicio 2.2 realizado por Jessica Diaz 0909-4212

--- While.cpp
+++ While.cpp
@@ -3,9 +3,8 @@ int main() {
     int numero = 0;
 
     while (numero <=30 || numero % 2 == 0){
-        numero +=3;
+        numero +=3
     }
-    std::cout << "El primer numero impar despues del 30 es: \n"<< numero << std::endl;
-    
+    std::cout << "El primer numero impar despues del 30 es: \n"<< numero << std::endl
     return 0;
 }
